4ccab37066dcf5ca85defb4b94550ca2a45231a7
Andrew Varnerin <andrew@varnerin.info>
2016-12-03 16:11:57 -0500

    show election data on map

--- build-data.sh
+++ build-data.sh
@@ -4,8 +4,9 @@ SOURCE_URL='http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_county_500k
 SHP_FILE='cb_2015_us_county_500k.shp'
 DBF_FILE='cb_2015_us_county_500k.dbf'
 MAPSHAPER='../node_modules/.bin/mapshaper'
+DATA_2016_URL='https://raw.githubusercontent.com/tonmcg/County_Level_Election_Results_12-16/master/2016_US_County_Level_Presidential_Results.csv'
 
-# download file
+# download data
 mkdir -p data
 cd data
 curl $SOURCE_URL >> source.zip
@@ -13,13 +14,21 @@ unzip -o source.zip $SHP_FILE $DBF_FILE
 mv $SHP_FILE data.shp
 mv $DBF_FILE data.dbf
 
+curl $DATA_2016_URL >> 2016_results.csv
+
 rm counties.json states.json 2> /dev/null
 
 # process data
 
-$MAPSHAPER data.shp -simplify 10% -o counties.json force id-field=GEOID format=geojson
+$MAPSHAPER data.shp -simplify 10% \
+  -each 'NUMERIC_ID=parseInt(GEOID, 10)' \
+  -join 2016_results.csv keys=NUMERIC_ID,combined_fips \
+  -filter-fields "total_votes,votes_dem,votes_gop,county_name,state_abbr" \
+  -o counties.json force id-field=GEOID format=geojson
+
 $MAPSHAPER data.shp -simplify 10% -dissolve STATEFP -o states.json force id-field=STATEFP format=geojson
 
 rm data.dbf data.shp source.zip
+rm 2016_results.csv
 
 cd ..
--- js/app.js
+++ js/app.js
@@ -1,18 +1,54 @@
 $(document).foundation();
 
+var demRepubScale = chroma.scale(['red', 'white', 'blue']).domain([-1, 0, 1]);
+
 (function(){
   var map;
 
   function init() {
     // create a new map with no base layer
     map = new L.Map('election-map', {
-      center: new L.LatLng(42.3964631, -71.1205171),
-      zoom: 16
+      center: new L.LatLng(42.3964631, -91.1205171),
+      zoom: 4
     });
+
+    var countiesPane = map.createPane('counties');
+    var statesPane = map.createPane('states');
 
     // use Stamen's 'terrain' base layer
     var layer = new L.StamenTileLayer('terrain');
     map.addLayer(layer);
+
+    jQuery.getJSON('data/counties.json', function(data) {
+      var countiesLayer = L.geoJSON(data, {
+        pane: 'counties',
+        style: function(feature) {
+          var p = feature.properties;
+          return {
+            fillOpacity: .9,
+            fillColor: demRepubScale((p.votes_dem - p.votes_gop) / p.total_votes * 2),
+            color: 'grey',
+            weight: 1
+          };
+        }
+      });
+      map.addLayer(countiesLayer);
+    });
+
+    jQuery.getJSON('data/states.json', function(data) {
+      var statesLayer = L.geoJSON(data, {
+        style: function(feature) {
+          return {
+            color: 'black',
+            weight: 1,
+            fillOpacity: 0
+          };
+        },
+        pane: 'states'
+      });
+
+      map.addLayer(statesLayer);
+    });
   }
 
   document.addEventListener('DOMContentLoaded', init);
