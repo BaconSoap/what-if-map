cbe60e7e6cd99ada5dea2f8a8ead34f9c1f6303a
Andrew Varnerin <andrew@varnerin.info>
2016-12-03 18:05:43 -0500

    add map interactivity

--- js/app.js
+++ js/app.js
@@ -2,8 +2,23 @@ $(document).foundation();
 
 var demRepubScale = chroma.scale(['red', 'white', 'blue']).domain([-1, 0, 1]);
 
-(function(){
+(function () {
   var map;
+  var countyInfo = L.control();
+  countyInfo.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'county-info');
+    this.update();
+    return this._div;
+  };
+
+  countyInfo.update = function (props) {
+    this._div.innerHTML = '<h6>Election Results</h6>' +
+      (props
+        ? props.county_name + ', ' + props.state_abbr +
+        '<br/>Dem: ' + props.votes_dem.toLocaleString() +
+        '<br/>GOP: ' + props.votes_gop.toLocaleString()
+        : '');
+  };
 
   function init() {
     // create a new map with no base layer
@@ -12,17 +27,23 @@ var demRepubScale = chroma.scale(['red', 'white', 'blue']).domain([-1, 0, 1]);
       zoom: 4
     });
 
-    var countiesPane = map.createPane('counties');
-    var statesPane = map.createPane('states');
-
     // use Stamen's 'terrain' base layer
     var layer = new L.StamenTileLayer('terrain');
     map.addLayer(layer);
 
-    jQuery.getJSON('data/counties.json', function(data) {
+    addCounties();
+    addStates();
+
+    countyInfo.addTo(map);
+  }
+
+  function addCounties() {
+    var countiesPane = map.createPane('counties');
+
+    jQuery.getJSON('data/counties.json', function (data) {
       var countiesLayer = L.geoJSON(data, {
         pane: 'counties',
-        style: function(feature) {
+        style: function (feature) {
           var p = feature.properties;
           return {
             fillOpacity: .9,
@@ -30,18 +51,54 @@ var demRepubScale = chroma.scale(['red', 'white', 'blue']).domain([-1, 0, 1]);
             color: 'grey',
             weight: 1
           };
+        },
+        onEachFeature: function (feature, layer) {
+          layer.on({
+            'mouseover': onHover,
+            'mouseout': onUnhover,
+            'click': onClick
+          })
         }
       });
+
       map.addLayer(countiesLayer);
     });
 
-    jQuery.getJSON('data/states.json', function(data) {
+    function onHover(e) {
+      var layer = e.target;
+      layer.setStyle({
+        weight: 5,
+        color: '#999'
+      });
+      countyInfo.update(layer.feature.properties);
+    }
+
+    function onUnhover(e) {
+      var layer = e.target;
+      layer.setStyle({
+        weight: 1,
+        color: 'grey'
+      });
+
+      countyInfo.update();
+    }
+
+    function onClick(e) {
+      map.fitBounds(e.target.getBounds());
+    }
+  }
+
+  function addStates() {
+    var statesPane = map.createPane('states');
+
+    jQuery.getJSON('data/states.json', function (data) {
       var statesLayer = L.geoJSON(data, {
-        style: function(feature) {
+        style: function (feature) {
           return {
             color: 'black',
             weight: 1,
-            fillOpacity: 0
+            fillOpacity: 0,
+            className: 'no-pointer-events'
           };
         },
         pane: 'states'
